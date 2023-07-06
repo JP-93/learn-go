f5f8bb50c6746aba90efb7e22a3d3e8640b0a923
JP-93 <pedrobsouza03@gmail.com>
2023-07-06 16:50:55 -0300

    add

--- algoritimo/problema1.go
+++ algoritimo/problema1.go
@@ -3,11 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var precos [5]int
-	precos[0], precos[1], precos[2], precos[3], precos[4] = 10, 9, 8, 7, 6
-	var menor [5]int
-
-	for i := 0; i < 5; i++ {
-	if precos[i] <
-	}
+	regioes := []int{1, 2, 3, 4, 5}
+	fmt.Println(regioes)
 }
+
+
